61c380554432228402deeb7281ef72486430060e
mtungij <jamesmtungi2013@gmail.com>
2024-06-19 13:27:48 +0300

    last one

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = 'https://loanpocket.phamasoft.com/'; 
-$config['base_url'] = 'http://localhost/sinza/';
+ $config['base_url'] = 'https://evamo-microfinance.phamasoft.com//';
+//$config['base_url'] = 'http://localhost/sinza/';
 
 
 
