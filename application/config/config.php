6f98e18392bd3f4ccef81727242e79a8125e6f5c
mtungij <jamesmtungi2013@gmail.com>
2025-07-07 13:51:34 +0300

    loan statement

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
- $config['base_url'] = 'http://localhost:8028/';
-//$config['base_url'] = 'http://localhost/sinza/';
+//  $config['base_url'] = 'http://localhost:8028/';
+$config['base_url'] = 'https://evamo.phamasoft.com/';
 
 
 
